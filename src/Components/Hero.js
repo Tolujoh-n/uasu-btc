e93db018b8e224513b5f7ee447ef6e77defc9552
Tolujoh-n <tolujohnofficial@gmail.com>
2023-09-26 17:23:48 +0100

    Faq remains

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,26 +1,45 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { initHeaderScrolled } from "../assets/js/headerScrolled";
-
+import image from "../Img/bitcoin-2.png";
+import image2 from "../Img/about-img.png";
+import image3 from "../Img/aboutimage.jpg";
 
 function Hero() {
   useEffect(() => {
     initHeaderScrolled();
   }, []);
   return (
-    <section id="hero" className="d-flex align-items-center justify-content-center">
+    <section
+      id="hero"
+      className="d-flex align-items-center justify-content-center"
+    >
       <div className="container" data-aos="fade-up">
-        <div className="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
+        <div
+          className="row justify-content-center"
+          data-aos="fade-up"
+          data-aos-delay="150"
+        >
           <div className="col-xl-12 col-lg-8">
-
-            <h1><span style={{ color: 'goldenrod' }}>PROTECT YOUR BITCOIN</span> FROM LOSSES WHILE <span style={{ color: 'goldenrod' }}>HOLDING YOUR KEYS</span></h1>
-            <h2>
-              Hedge using unstoppable bitcoin payoff on Layer 1
-            </h2>
+            <h1>
+              <span style={{ color: "goldenrod" }}>PROTECT YOUR BITCOIN</span>{" "}
+              FROM LOSSES WHILE{" "}
+              <span style={{ color: "goldenrod" }}>HOLDING YOUR KEYS</span>
+            </h1>
+            <h2>Hedge using unstoppable bitcoin payoff on Layer 1</h2>
             <br></br>
             <Link to="/Waitlist" className="api-started-btn scrollto">
               Join Alpha Waiting list
             </Link>
+            <br></br>
+            <div className="join-count">
+              <div className="join-images">
+                <img src={image} alt="Person 1" className="join-image" />
+                <img src={image2} alt="Person 2" className="join-image" />
+                <img src={image3} alt="Person 3" className="join-image" />
+              </div>
+              <p>Join 100+ Bitcoiners</p>
+            </div>
           </div>
         </div>
       </div>
--- src/Components/Waitlist.js
+++ src/Components/Waitlist.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { initHeaderScrolled } from "../assets/js/headerScrolled";
+import ReCAPTCHA from "react-google-recaptcha";
 
 function Waitlist() {
   const [email, setEmail] = useState("");
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -11,8 +13,15 @@ function Waitlist() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Here, you can add logic to submit the email to your backend or perform any other actions.
+
+    if (!recaptchaValue) {
+      alert("Please complete the reCAPTCHA.");
+      return;
+    }
+
+    //logic to submit the email and reCAPTCHA value to the backend or performing any other actions.
     console.log("Email submitted:", email);
+    console.log("reCAPTCHA value:", recaptchaValue);
   };
 
   useEffect(() => {
@@ -32,7 +41,10 @@ function Waitlist() {
         >
           <div className="col-xl-12 col-lg-8">
             <section className="waitlist-section">
-              <h1><span style={{ color: 'goldenrod' }}>Join</span> the <span style={{ color: 'goldenrod' }}>Waitlist</span></h1>
+              <h1>
+                <span style={{ color: "goldenrod" }}>Join</span> the{" "}
+                <span style={{ color: "goldenrod" }}>Waitlist</span>
+              </h1>
               <h2>Submit your email to get on our waitlist.</h2>
               <form onSubmit={handleSubmit}>
                 <input
@@ -41,6 +53,10 @@ function Waitlist() {
                   value={email}
                   onChange={handleEmailChange}
                   required
+                />
+                <ReCAPTCHA
+                  sitekey="YOUR_RECAPTCHA_SITE_KEY" // Replace with your reCAPTCHA site key
+                  onChange={(value) => setRecaptchaValue(value)}
                 />
                 <button type="submit" className="submit-button">
                   Submit
